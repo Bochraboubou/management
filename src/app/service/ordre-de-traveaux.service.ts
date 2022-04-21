0c14f019d9a9abc8694be00280c0154060d2114e
Bochra Nahali <bochra.nahalii@gmail.com>
2022-04-21 12:44:29 +0100

    montant total ot

--- src/app/service/ordre-de-traveaux.service.ts
+++ src/app/service/ordre-de-traveaux.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdreDeTraveauxService {
+  private apiServeUrl =environment.apiBaseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  
+  //récuperer le total des montant des OT
+  public getArticlebyCodeandMetierId(): Observable<number>
+  {
+    return this.http.get<number>(`${this.apiServeUrl}/admin/montantTotalOT`);
+  }
 }
